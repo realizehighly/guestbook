f2075bfd7ac6b159395c87acc350e475bc89bc70
Heonjun Jeong <gjswns1991@gmail.com>
2022-02-16 18:26:05 +0900

    목록처리(여러 게시판 재사용 가능)

--- src/main/java/org/zerock/guestbook/service/GuestbookService.java
+++ src/main/java/org/zerock/guestbook/service/GuestbookService.java
@@ -1,12 +1,16 @@
 package org.zerock.guestbook.service;
 
 import org.zerock.guestbook.dto.GuestbookDTO;
+import org.zerock.guestbook.dto.PageRequestDTO;
+import org.zerock.guestbook.dto.PageResultDTO;
 import org.zerock.guestbook.entity.Guestbook;
 
 public interface GuestbookService {
 
     //새로운 방명록을 등록
     Long register(GuestbookDTO dto);
+
+    PageResultDTO<GuestbookDTO,Guestbook> getList(PageRequestDTO requestDTO);
 
     default Guestbook dtoToEntity(GuestbookDTO dto){
         Guestbook entity = Guestbook.builder()
@@ -15,7 +19,20 @@ public interface GuestbookService {
                 .content(dto.getContent())
                 .writer(dto.getWriter())
                 .build();
-
         return entity;
+    }
+
+    default GuestbookDTO entityToDto(Guestbook entity){
+
+        GuestbookDTO dto = GuestbookDTO.builder()
+                .gno(entity.getGno())
+                .title(entity.getTitle())
+                .content(entity.getContent())
+                .writer(entity.getWriter())
+                .regDate(entity.getRegDate())
+                .modDate(entity.getModDate())
+                .build();
+
+        return dto;
     }
 }

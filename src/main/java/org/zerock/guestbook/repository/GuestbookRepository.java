2a303915f886026dc7eed52f82a060f28d6b736a
Heonjun Jeong <gjswns1991@gmail.com>
2022-02-23 20:56:38 +0900

    방명록의 검색처리

--- src/main/java/org/zerock/guestbook/repository/GuestbookRepository.java
+++ src/main/java/org/zerock/guestbook/repository/GuestbookRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.zerock.guestbook.entity.Guestbook;
 
 public interface GuestbookRepository extends JpaRepository<Guestbook, Long> ,
+
         QuerydslPredicateExecutor<Guestbook> {
 }
